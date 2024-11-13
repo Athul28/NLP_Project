83c2769306a7d79087c7693a6afe5f29a73ef72f
Athul Bhandary <athulbhandary28@gmail.com>
2024-11-13 13:02:31 +0530

    feat: find keywords

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer=SentimentIntensityAnalyzer()
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+from nltk.corpus import stopwords
+import numpy as np
 
 app=Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
@@ -26,6 +30,9 @@ def predictComments():
 
 
         #add any extra code here
+
+
+        #Sentiment analysis
         positive=0
         negative=0
         neutral=0
@@ -42,6 +49,41 @@ def predictComments():
                 neutral+=1
 
 
+
+        #Keywords extraction
+        stop_words = stopwords.words('english')
+
+        def preprocess_text(text):
+            processed_text = []
+            for t in text:
+                t = t.lower()  
+                t = re.sub(r'[^\w\s]', '', t)   
+                processed_text.append(t)
+            return processed_text
+
+        # Preprocess the comments
+        processed_comments = preprocess_text(reviews)
+
+        vectorizer = CountVectorizer(stop_words=stop_words, ngram_range=(1, 2)) 
+        X = vectorizer.fit_transform(processed_comments)
+
+        terms = vectorizer.get_feature_names_out()
+
+        word_frequencies = X.toarray().sum(axis=0)
+
+        frequency_dict = {term: word_frequencies[i] for i, term in enumerate(terms)}
+
+        sorted_frequency = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
+
+        keywords=[]
+
+        # Display top 50 most frequent words (and bigrams)
+        for term, freq in sorted_frequency[:50]:
+            keywords.append(f"{term}: {int(freq)}")
+            print(f"{term}: {freq}")
+
+
+
         response={
             "app_id":app_id,
             "Reviews":reviews,
@@ -49,7 +91,8 @@ def predictComments():
                 "positive":positive,
                 "negative":negative,
                 "neutral":neutral
-            }
+            },
+            "Key Words":keywords
         }
 
         return jsonify(response)
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import LoadingIcons from 'react-loading-icons'
+
+
 
 export default function Home() {
   const [positive,setPositive]=useState(0)
   const [negative,setNegative]=useState(0)
   const [neutral,setNeutral]=useState(0)
   const [loading,setLoading]=useState(false)
+  const [keyWords,setKeyWords]=useState<string[]>([])
 
 
   const getComments = async() => {
-    const youtubeLink = (document.querySelector('input[type="text"]') as HTMLInputElement).value;
+    const id = (document.querySelector('input[type="text"]') as HTMLInputElement).value;
     const noOfComments = (document.querySelector('input[type="number"]') as HTMLInputElement).value;
 
     setLoading(true)
@@ -21,40 +25,59 @@ export default function Home() {
       'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-      "url":youtubeLink,
+      "url":id,
       "noComments":noOfComments
       })
     });
 
     const data = await response.json();
+    console.log(data)
     console.log(data["Sentimental Analysis"]);
     const sentiments=data["Sentimental Analysis"]
     setPositive(sentiments.positive)
     setNegative(sentiments.negative)
     setNeutral(sentiments.neutral)
     setLoading(false)
+    setKeyWords(data["Key Words"])
   };
 
   return (
     <div>
-      <p>Enter the inputs : </p>
+      <div className="border border-white p-10 m-auto w-fit rounded-lg mt-10">
+      <p className="text-xl">Google Play Store reviews analysis</p>
+      <br />
       <label htmlFor="youtube-link">Enter the app id : </label>
-      <input type="text" className="text-black"/>
+      <input type="text" className="text-black p-2 rounded-xl" />
       <br />
       <br />
       <label htmlFor="no-comments">No of reviews : </label>
-      <input type="number" className="text-black"/>
+      <input type="number" className="text-black p-2 rounded-xl" />
       <br />
-      <button onClick={getComments}>Submit</button>
+      <div className="w-fit m-auto">
+
+      <button onClick={getComments} className="bg-purple-700 px-4 py-3 mt-3 rounded-md">Submit</button>
+      </div>
+      </div>
       {!loading ? (
-        <div>
-          <p>Sentiments</p>
-          <p>Positive : {positive}</p>
-          <p>Negative : {negative}</p>
-          <p>Neutral : {neutral}</p>
+      <div>
+        <div className="border border-white w-fit m-auto mt-5 p-5 flex flex-col items-center">
+        <p className="text-xl font-bold underline">Sentiments</p>
+        <p className="text-green-500">Positive : {positive}</p>
+        <p className="text-red-500">Negative : {negative}</p>
+        <p className="text-blue-400">Neutral : {neutral}</p>
         </div>
+
+        <div className="p-5">
+          <p className="text-center my-5 text-xl font-bold underline">Key Words:</p>
+          <ul className="flex flex-wrap">
+          {keyWords.map((word, index) => (
+            <li key={index} className=" bg-slate-700 m-2 p-3">{word}</li>
+          ))}
+          </ul>
+        </div>
+      </div>
       ) : (
-        <p>Loading</p>
+      <div className="w-fit m-auto"><LoadingIcons.Bars /></div>
       )}
     </div>
   );
